66a8812cbc29f50cf021720aac3c0a84a5d267b3
Carl Persson <carl.persson@mac.com>
2014-04-15 16:20:54 +0200

    Added Util.pasteHtmlAtCaret and Util.pasteTextAtCaret

--- tiki/js/util.js
+++ tiki/js/util.js
@@ -16,6 +16,45 @@ define([
             return m;
         });
     };
+
+    util.pasteHtmlAtCaret = function(html) {
+        var sel, range;
+        if(window.getSelection) {
+            sel = window.getSelection();
+            if(sel.getRangeAt && sel.rangeCount) {
+                range = sel.getRangeAt(0);
+                range.deleteContents();
+
+                var el = document.createElement("div");
+                el.innerHTML = html;
+                var frag = document.createDocumentFragment(), node, lastNode;
+                while((node = el.firstChild)) {
+                    lastNode = frag.appendChild(node);
+                }
+                range.insertNode(frag);
+
+                // Preserve the selection
+                if(lastNode) {
+                    range = range.cloneRange();
+                    range.setStartAfter(lastNode);
+                    range.collapse(true);
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+                }
+            }
+        } 
+        else if(document.selection && document.selection.type != "Control") {
+            // ie<9
+            document.selection.createRange().pasteHTML(html);
+        }
+    };
+    util.pasteTextAtCaret = function(text) {
+        var el = document.createTextNode(text);
+        util.pasteHtmlAtCaret(el.data);
+    };
+
+
+
     util.getClosestStartingWith = function(collection, text, textAttr) {
             if(!text.length)
                 return;
